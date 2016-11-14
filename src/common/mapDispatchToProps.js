47a5821eca960f67837b9bad4a66d75551d5a10d
Joshua Gross <joshua.gross@gmail.com>
2016-11-13 17:19:16 -0800

    document mapDispatchToProps function

--- src/common/mapDispatchToProps.js
+++ src/common/mapDispatchToProps.js
@@ -1,6 +1,11 @@
 import zipObject from 'lodash/zipObject';
 import { bindActionCreators } from 'redux';
 
+/**
+ * Pass in a dict of dicts of actions, and then a dispatch function. Returns
+ * the same dictionary but with action producing functions bound to the `dispatch`
+ * function, so that components can dispatch actions to the redux store.
+ */
 export default function mapDispatchToProps (actions) {
   return (dispatch) => {
     const actionKeys = Object.keys(actions);
